ea68627483aecbeda44ea05cae50afd70a6c085c
KTachibanaM <whj19931115@gmail.com>
2020-03-08 11:17:24 -0700

    fa

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 pushd /home/mastodon/live
-git apply -R /home/mastodon/mastodon-issues/higher-throttle.diff
+git apply -R /home/mastodon/mastodon-issues/higher-throttle.diff || echo "this is idempotent"
 git apply /home/mastodon/mastodon-issues/higher-throttle.diff
 
-git apply -R /home/mastodon/mastodon-issues/higher-feed-count.diff
+git apply -R /home/mastodon/mastodon-issues/higher-feed-count.diff || echo "this is idempotent"
+
+git status
 popd
