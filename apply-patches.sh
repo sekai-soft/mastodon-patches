dbbdda908b97e2c97789a7957f12e78975fd87b3
KTachibanaM <whj19931115@gmail.com>
2020-03-08 11:16:06 -0700

    fa

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 pushd /home/mastodon/live
+git apply -R /home/mastodon/mastodon-issues/higher-throttle.diff
 git apply /home/mastodon/mastodon-issues/higher-throttle.diff
-git apply /home/mastodon/mastodon-issues/higher-feed-count.diff
+
 git apply -R /home/mastodon/mastodon-issues/higher-feed-count.diff
 popd
