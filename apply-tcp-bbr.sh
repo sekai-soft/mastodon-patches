96a0a04757947edfb2449222694ba92a21f2484d
KTachibanaM <whj19931115@gmail.com>
2020-04-03 11:37:48 -0700

    fu

--- apply-tcp-bbr.sh
+++ apply-tcp-bbr.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 if grep -Fxq "net.core.default_qdisc=fq" /etc/sysctl.conf
 then
     echo "net.core.default_qdisc=fq exists"
 else
-    sudo echo -e "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+    echo -e "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 fi
 
 if grep -Fxq "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf
 then
     echo "net.ipv4.tcp_congestion_control=bbr exists"
 else
-    sudo echo -e "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+    echo -e "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 fi
