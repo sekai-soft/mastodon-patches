eb94fe4fb4d6302047fa8af13b7092ab3ea1ccd5
KTachibanaM <whj19931115@gmail.com>
2020-04-05 10:29:42 -0700

    specify mastodon path

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-pushd /home/mastodon/live
+if [[ -z "${MASTODON_PATH}" ]]; then
+  echo "MASTODON_PATH is undefined"
+  exit 1
+else
+
+pushd "${MASTODON_PATH}"
 
 git apply -R /home/mastodon/mastodon-issues/higher-throttle.diff || echo "this is idempotent"
 git apply /home/mastodon/mastodon-issues/higher-throttle.diff
