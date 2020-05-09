1a1bbcc59380e23d6cceb5a313e64568dffa7a60
KTachibanaM <whj19931115@gmail.com>
2020-05-09 11:53:47 -0700

    fa

--- backup-pg.sh
+++ backup-pg.sh
@@ -7,6 +7,13 @@ if [[ -z "${PG_BACKUP_ROOT}" ]]; then
 fi
 
 DATE=`date '+%Y-%m-%d-%H-%M-%S'`
+BACKUP_TAR="${DATE}.tar"
 
-pg_dump -v -f "${PG_BACKUP_ROOT}/${DATE}.tar" -F tar -h /var/run/postgresql -p 5432 -d mastodon_production -U mastodon
+pg_dump -v -f "${PG_BACKUP_ROOT}/${BACKUP_TAR}" -F tar -h /var/run/postgresql -p 5432 -d mastodon_production -U mastodon
 ls -alh "${PG_BACKUP_ROOT}"
+
+for f in *; do
+  if [ "${f}" != "${BACKUP_TAR}" ]; then
+    rm ${f}
+  fi
+done
