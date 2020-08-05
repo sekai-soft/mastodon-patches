6c5da468b9a5da1ba5c5d259eae3fca2e3fec631
KTachibanaM <whj19931115@gmail.com>
2020-08-05 15:39:57 -0700

    more cleaning measures

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-RAILS_ENV=production /home/mastodon/live/bin/tootctl media remove --days 7
+RAILS_ENV=production /home/mastodon/live/bin/tootctl media remove --days 3
+RAILS_ENV=production /home/mastodon/live/bin/tootctl preview_cards remove --days 14
+RAILS_ENV=production /home/mastodon/live/bin/tootctl statuses remove --days 14
