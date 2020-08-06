def66b4b51ec2a417349d0d1054be05f264061a1
KTachibanaM <whj19931115@gmail.com>
2020-08-05 17:13:06 -0700

    oops

--- cleanup.sh
+++ cleanup.sh
@@ -4,4 +4,4 @@ set -e
 RAILS_ENV=production /home/mastodon/live/bin/tootctl media remove --days 3
 RAILS_ENV=production /home/mastodon/live/bin/tootctl media remove-orphans
 RAILS_ENV=production /home/mastodon/live/bin/tootctl preview_cards remove --days 14
-RAILS_ENV=production /home/mastodon/live/bin/tootctl statuses remove --days 14
+# RAILS_ENV=production /home/mastodon/live/bin/tootctl statuses remove --days 14
