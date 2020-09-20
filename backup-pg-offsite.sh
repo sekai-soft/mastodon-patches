db474851c52a2886b0adeaa54c0d5e77c2a36836
KTachibanaM <whj19931115@gmail.com>
2020-09-20 11:14:38 -0700

    offsite not suing

--- backup-pg-offsite.sh
+++ backup-pg-offsite.sh
@@ -11,6 +11,6 @@ if [[ -z "${PG_BACKUP_ROOT}" ]]; then
   exit 1
 fi
 
-ssh "${SSH_USERNAME_AND_HOST}" "su - mastodon -c 'PG_BACKUP_ROOT=${PG_BACKUP_ROOT} /home/mastodon/mastodon-issues/backup-pg.sh'"
+ssh "${SSH_USERNAME_AND_HOST}" "PG_BACKUP_ROOT=${PG_BACKUP_ROOT} /home/mastodon/mastodon-issues/backup-pg.sh"
 BACKUP_FILENAME=$(ssh "${SSH_USERNAME_AND_HOST}" "ls ${PG_BACKUP_ROOT} -rt | tail -1")
 scp "${SSH_USERNAME_AND_HOST}":"${PG_BACKUP_ROOT}"/"${BACKUP_FILENAME}" .
