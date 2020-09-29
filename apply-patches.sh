c19f40bdb67713148559606ad8ba2bb1c1802ca6
Frederick Zhang <frederick888@tsundere.moe>
2020-09-30 00:36:16 +1000

    bump preview image size limit to 10M

--- apply-patches.sh
+++ apply-patches.sh
@@ -19,6 +19,9 @@ git apply "${CURRENT_PWD}/higher-feed-count.diff"
 git apply -R "${CURRENT_PWD}/pixiv-preview-with-embed-url.diff" || echo "this is idempotent"
 git apply "${CURRENT_PWD}/pixiv-preview-with-embed-url.diff"
 
+git apply -R "${CURRENT_PWD}/higher-preview-size-limit.diff" || echo "this is idempotent"
+git apply "${CURRENT_PWD}/higher-preview-size-limit.diff"
+
 git status
 git diff
 
