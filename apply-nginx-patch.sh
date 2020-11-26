2041fc79437b815c72af621a04336113dff0a73e
KTachibanaM <whj19931115@gmail.com>
2020-11-26 15:32:15 -0800

    hope this works lol

--- apply-nginx-patch.sh
+++ apply-nginx-patch.sh
@@ -6,14 +6,6 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-CURRENT_PWD=$(pwd)
-
-if [ ! -f /etc/nginx/sites-available/mastodon.init ]; then
-  echo "mastodon.init doesn't exist, creating one"
-  cp /etc/nginx/sites-available/mastodon /etc/nginx/sites-available/mastodon.init
-fi
-
-cp /etc/nginx/sites-available/mastodon.init /etc/nginx/sites-available/mastodon
 patch -N -r- /etc/nginx/sites-available/mastodon nginx.patch || echo "this is idempotent"
 
 nginx -t
