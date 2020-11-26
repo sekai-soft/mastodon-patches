8157d624f1d1c9e452a2aed8f305e3d47a936f46
KTachibanaM <whj19931115@gmail.com>
2020-11-26 15:10:55 -0800

    ban sites

---
+++ apply-nginx-patch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+CURRENT_PWD=$(pwd)
+
+if [ ! -f /etc/nginx/sites-available/mastodon.init ]; then
+  echo "mastodon.init doesn't exist, creating one"
+  cp /etc/nginx/sites-available/mastodon /etc/nginx/sites-available/mastodon.init
+fi
+
+cp /etc/nginx/sites-available/mastodon.init /etc/nginx/sites-available/mastodon
+patch -N -r- /etc/nginx/sites-available/mastodon "${CURRENT_PWD}/nginx.patch" || echo "this is idempotent"
+
+nginx -t
+systemctl reload nginx
