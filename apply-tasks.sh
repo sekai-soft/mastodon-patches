6143847ce47de8545da55ac1b84ac3a5aa4e6d6e
KTachibanaM <whj19931115@gmail.com>
2022-02-22 22:36:36 -0800

    add timer for open reg

--- apply-tasks.sh
+++ apply-tasks.sh
@@ -3,5 +3,7 @@ set -e
 
 sudo cp ./tasks/mastodon-cleanup.timer /etc/systemd/system
 sudo cp ./tasks/mastodon-cleanup.service /etc/systemd/system
+sudo cp ./tasks/mastodon-open.timer /etc/systemd/system
+sudo cp ./tasks/mastodon-open.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable --now mastodon-cleanup.timer
+sudo systemctl enable --now mastodon-cleanup.timer mastodon-open.timer
