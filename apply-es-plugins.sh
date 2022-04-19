5b2660ea0aad7b5c04995bda19c45774706c0e03
KTachibanaM <whj19931115@gmail.com>
2022-04-19 10:16:30 -0700

    sudo

--- apply-es-plugins.sh
+++ apply-es-plugins.sh
@@ -4,12 +4,12 @@ set -e
 ES_VERSION=7.17.2
 
 echo Installing analysis-ik
-    /usr/share/elasticsearch/bin/elasticsearch-plugin remove analysis-ik || echo analysis-ik not installed
-/usr/share/elasticsearch/bin/elasticsearch-plugin install -b https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v$ES_VERSION/elasticsearch-analysis-ik-$ES_VERSION.zip
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin remove analysis-ik || echo analysis-ik not installed
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v$ES_VERSION/elasticsearch-analysis-ik-$ES_VERSION.zip
 
 echo Installing analysis-stconvert
-/usr/share/elasticsearch/bin/elasticsearch-plugin remove analysis-stconvert || echo analysis-stconvert not installed
-/usr/share/elasticsearch/bin/elasticsearch-plugin install -b https://github.com/medcl/elasticsearch-analysis-stconvert/releases/download/v$ES_VERSION/elasticsearch-analysis-stconvert-$ES_VERSION.zip
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin remove analysis-stconvert || echo analysis-stconvert not installed
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b https://github.com/medcl/elasticsearch-analysis-stconvert/releases/download/v$ES_VERSION/elasticsearch-analysis-stconvert-$ES_VERSION.zip
 
 echo Restarting elasticsearch
 sudo systemctl restart elasticsearch
