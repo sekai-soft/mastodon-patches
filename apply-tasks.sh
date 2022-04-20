1608a2ffc3b1a84f501762a8722c69c13d794ce3
KTachibanaM <whj19931115@gmail.com>
2022-04-19 22:30:52 -0700

    remove reg open and close timers

--- apply-tasks.sh
+++ apply-tasks.sh
@@ -3,9 +3,5 @@ set -e
 
 sudo cp ./tasks/mastodon-cleanup.timer /etc/systemd/system
 sudo cp ./tasks/mastodon-cleanup.service /etc/systemd/system
-sudo cp ./tasks/mastodon-open.timer /etc/systemd/system
-sudo cp ./tasks/mastodon-open.service /etc/systemd/system
-sudo cp ./tasks/mastodon-close.timer /etc/systemd/system
-sudo cp ./tasks/mastodon-close.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable --now mastodon-cleanup.timer mastodon-open.timer mastodon-close.timer
+sudo systemctl enable --now mastodon-cleanup.timer
