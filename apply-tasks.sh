4b1ecbff45c432301e61f4852335ea875d272298
KTachibanaM <whj19931115@gmail.com>
2022-09-12 19:05:14 -0700

    es hc

--- apply-tasks.sh
+++ apply-tasks.sh
@@ -5,5 +5,7 @@ sudo cp ./tasks/mastodon-cleanup.timer /etc/systemd/system
 sudo cp ./tasks/mastodon-cleanup.service /etc/systemd/system
 sudo cp ./tasks/mastodon-backup.timer /etc/systemd/system
 sudo cp ./tasks/mastodon-backup.service /etc/systemd/system
+sudo cp ./tasks/es-healthcheck.timer /etc/systemd/system
+sudo cp ./tasks/es-healthcheck.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable --now mastodon-cleanup.timer mastodon-backup.timer
+sudo systemctl enable --now mastodon-cleanup.timer mastodon-backup.timer es-healthcheck.timer
