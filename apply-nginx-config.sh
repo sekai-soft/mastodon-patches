859e253439e2f9e84af069fdf2f47d4c6499c136
KTachibanaM <whj19931115@gmail.com>
2023-07-06 15:47:30 -0700

    add files host

--- apply-nginx-config.sh
+++ apply-nginx-config.sh
@@ -2,6 +2,8 @@
 set -e
 
 sudo cp ./assets/nginx.conf /etc/nginx/sites-available/mastodon
+sudo cp ./assets/files.nginx.conf /etc/nginx/sites-available/files
+sudo ln -s /etc/nginx/sites-available/files /etc/nginx/sites-enabled/
 
 echo "Checking nginx syntax..."
 sudo nginx -t
