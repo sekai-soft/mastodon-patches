5c716062cfa1c5af95698295b08c7e5d345fa760
KTachibanaM <whj19931115@gmail.com>
2023-07-06 15:52:09 -0700

    duh

--- apply-nginx-config.sh
+++ apply-nginx-config.sh
@@ -3,7 +3,6 @@ set -e
 
 sudo cp ./assets/nginx.conf /etc/nginx/sites-available/mastodon
 sudo cp ./assets/files.nginx.conf /etc/nginx/sites-available/files
-sudo ln -s /etc/nginx/sites-available/files /etc/nginx/sites-enabled/
 
 echo "Checking nginx syntax..."
 sudo nginx -t
