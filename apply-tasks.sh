a429f804d9229f2ed708dc800447bc713405ff71
KTachibanaM <whj19931115@gmail.com>
2023-07-06 19:18:10 -0700

    scheduled reboot on sundays

--- apply-tasks.sh
+++ apply-tasks.sh
@@ -7,5 +7,6 @@ sudo cp ./assets/mastodon-backup.timer /etc/systemd/system
 sudo cp ./assets/mastodon-backup.service /etc/systemd/system
 sudo cp ./assets/mastodon-es-healthcheck.timer /etc/systemd/system
 sudo cp ./assets/mastodon-es-healthcheck.service /etc/systemd/system
+sudo cp ./assets/reboot.timer /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable --now mastodon-cleanup.timer mastodon-backup.timer mastodon-es-healthcheck.timer
+sudo systemctl enable --now mastodon-cleanup.timer mastodon-backup.timer mastodon-es-healthcheck.timer reboot.timer
